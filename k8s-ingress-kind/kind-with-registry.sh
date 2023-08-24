f9ba5bcb70e6a15ec85607f9eb373838604230b2
Auri Munoz <amunozhe@redhat.com>
2023-08-24 08:24:31 +0200

    remove wait for ready status

--- k8s-ingress-kind/kind-with-registry.sh
+++ k8s-ingress-kind/kind-with-registry.sh
@@ -81,7 +81,7 @@ EOF
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 
 #Wait until is ready to process requests running:
-kubectl wait --namespace ingress-nginx \
-  --for=condition=ready pod \
-  --selector=app.kubernetes.io/component=controller \
-  --timeout=90s
+#kubectl wait --namespace ingress-nginx \
+#  --for=condition=ready pod \
+#  --selector=app.kubernetes.io/component=controller \
+#  --timeout=90s
